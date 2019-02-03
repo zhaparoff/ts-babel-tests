6aa8b1b5a62ed22db2e6efd74a402275b127dc8d
Anton Zhaparov <zhaparoff@gmail.com>
2019-02-03 19:11:58 +0200

    JS test added

---
+++ src/js/babelTest3.js
@@ -0,0 +1,16 @@
+// @ts-nocheck
+// JS: Array.prototype.forEach() is called for the same scope array
+
+function logCollectionInternal(array) {
+    if (array == null) {
+        throw new Error("Collection is required.");
+    }
+
+    array.forEach(function (item) {
+        console.log(item);
+    });
+}
+
+export function logCollection() {
+    logCollectionInternal(["1", "2", "3"]);
+}
